bfed701e0b40c6968d28869325237660a8865094
JanceGeronimo <124651783+JanceGeronimo@users.noreply.github.com>
2023-02-10 13:02:04 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+const sr = ScrollReveal ({
+    distance: '60px',
+    duration: 2500,
+    reset: true
+})
+
+sr.reveal('.home-text', {delay:50, origin: 'top'})
+sr.reveal('.home-img', {delay:50, origin: 'top'})
+sr.reveal('.about, .hobbies, .social, .copyright', {delay: 50, origin: 'top'})
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
